cf2db8ba903371fa2e57e0a83e78d52a811e7a2e
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2024-12-23 17:57:29 +0530

    added homework questions of 20-12-2024

--- src/Array_Assignment1/ReverseAnArrayInMemory.java
+++ src/Array_Assignment1/ReverseAnArrayInMemory.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 
 public class ReverseAnArrayInMemory {
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50,60,70,80,90};
+        int[] arr = {10,20,30,40,50,60,70};
+//        method:1
         int index=0;
         for(int i=arr.length-1;i>=arr.length/2;i--){
             int temp = arr[index];
@@ -14,5 +15,17 @@ public class ReverseAnArrayInMemory {
             index++;
         }
         System.out.println(Arrays.toString(arr));
+
+//        method:2
+//        int first = 0;
+//        int last = arr.length-1;
+//        while (last>first){
+//            int temp=arr[first];
+//            arr[first]=arr[last];
+//            arr[last]=temp;
+//            last--;
+//            first++;
+//        }
+//        System.out.println(Arrays.toString(arr));
     }
 }
