bad88b448ea3f43dc1b456b561c175ce4e9f023e
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2024-12-26 18:55:23 +0530

    added todays practice problems

---
+++ src/Array_Assignment2/Question5.java
@@ -0,0 +1,18 @@
+package Array_Assignment2;
+
+public class Question5 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,6,3};
+        int currentNum=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(currentNum>arr[i]){
+                System.out.println("false");
+                return;
+            }else{
+                currentNum=arr[i];
+            }
+        }
+        System.out.println("true");
+    }
+}
