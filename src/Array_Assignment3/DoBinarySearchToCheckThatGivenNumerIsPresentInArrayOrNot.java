e1016d51ea7128bd6a3c501954dc719a2de495bf
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2025-01-02 18:02:34 +0530

    added binary search problem

---
+++ src/Array_Assignment3/DoBinarySearchToCheckThatGivenNumerIsPresentInArrayOrNot.java
@@ -0,0 +1,23 @@
+package Array_Assignment3;
+
+public class DoBinarySearchToCheckThatGivenNumerIsPresentInArrayOrNot {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80};
+        int k=30;
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            System.out.println("Hello");
+            int middle=(left+right)/2;
+            if(arr[middle]==k){
+                System.out.println("Present");
+                return;
+            }else if(arr[middle]<k){
+                left=middle+1;
+            }else{
+                right=middle-1;
+            }
+        }
+        System.out.println("Not Present");
+    }
+}
