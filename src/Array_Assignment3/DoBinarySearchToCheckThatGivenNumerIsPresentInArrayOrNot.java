f4a6470bee9b16325bfa55b68184afbba87021c6
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2025-01-06 15:27:54 +0530

    made changes in binary search

--- src/Array_Assignment3/DoBinarySearchToCheckThatGivenNumerIsPresentInArrayOrNot.java
+++ src/Array_Assignment3/DoBinarySearchToCheckThatGivenNumerIsPresentInArrayOrNot.java
@@ -2,12 +2,11 @@ package Array_Assignment3;
 
 public class DoBinarySearchToCheckThatGivenNumerIsPresentInArrayOrNot {
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50,60,70,80};
+        int[] arr = {10,20,300,40,50,60,70,80};
         int k=30;
         int left=0;
         int right=arr.length-1;
         while(left<=right){
-            System.out.println("Hello");
             int middle=(left+right)/2;
             if(arr[middle]==k){
                 System.out.println("Present");
