2a48c49e27920350778a86fb2de711b3e15100d0
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2025-02-14 13:46:41 +0530

    modified question no 5 of assignment 2 in array

--- src/Array/Array_Assignment2/Question5.java
+++ src/Array/Array_Assignment2/Question5.java
@@ -3,14 +3,22 @@ package Array.Array_Assignment2;
 public class Question5 {
     public static void main(String[] args) {
         int[] arr = {1,2,6,3};
-        int currentNum=arr[0];
+//        int currentNum=arr[0];
+//
+//        for(int i=1;i<arr.length;i++){
+//            if(currentNum>arr[i]){
+//                System.out.println("false");
+//                return;
+//            }
+//            currentNum=arr[i];
+//        }
+//        System.out.println("true");
 
         for(int i=1;i<arr.length;i++){
-            if(currentNum>arr[i]){
+            if(arr[i]<arr[i-1]){
                 System.out.println("false");
                 return;
             }
-            currentNum=arr[i];
         }
         System.out.println("true");
     }
