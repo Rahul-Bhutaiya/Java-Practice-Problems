13ca8415a31982addd309bb9243b0d946390f7f6
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2025-03-08 18:21:04 +0530

    added string-builder practice code

---
+++ src/String/demo.java
@@ -0,0 +1,7 @@
+package String;
+
+public class demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/StringBuilder/test.java
@@ -0,0 +1,26 @@
+package StringBuilder;
+
+public class test {
+    public static void main(String[] args) {
+        StringBuilder s1= new StringBuilder("Rahul");
+
+//        System.out.println(s1.length());//5
+
+//        System.out.println(s1.charAt(2));//h
+
+//        s1.setCharAt(1,'o');
+//        System.out.println(s1);//Rohul
+
+//        s1.insert(0,"P");
+//        System.out.println(s1);//PRahul
+
+//        s1.delete(0,1);
+//        System.out.println(s1);//ahul
+//        s1.delete(0,3);
+//        System.out.println(s1);//ul
+
+//        s1.append(" Bhutaiya");
+//        System.out.println(s1);//Rahul Bhutaiya
+
+    }
+}
