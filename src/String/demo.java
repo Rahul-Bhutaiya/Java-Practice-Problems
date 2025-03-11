3b1979f16bb177e9ae9998fbe05787e611c6e1c5
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2025-03-11 11:50:48 +0530

    added StringBuilder pdf and modified String pfd

--- src/String/demo.java
+++ src/String/demo.java
@@ -2,6 +2,9 @@ package String;
 
 public class demo {
     public static void main(String[] args) {
+        String[]  words = {"abc","car","ada","racecar","cool"};
 
+
+        System.out.println((char)98);
     }
 }
--- src/StringBuilder/test.java
+++ src/StringBuilder/test.java
@@ -4,10 +4,13 @@ public class test {
     public static void main(String[] args) {
         StringBuilder s1= new StringBuilder("Rahul");
 
+//        StringBuilder s2= new StringBuilder("Rahul");
+//        System.out.println(s1==s2);//false
+
 //        System.out.println(s1.length());//5
 
 //        System.out.println(s1.charAt(2));//h
-
+//
 //        s1.setCharAt(1,'o');
 //        System.out.println(s1);//Rohul
 
@@ -21,6 +24,8 @@ public class test {
 
 //        s1.append(" Bhutaiya");
 //        System.out.println(s1);//Rahul Bhutaiya
+
+//        System.out.println(s1.toString());//Rahul //converts StringBuilder into String
 
     }
 }
