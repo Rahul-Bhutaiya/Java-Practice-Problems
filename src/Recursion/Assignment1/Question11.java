535e5398c95e9c4da4f6b3d92d08647b27ac2bde
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2025-05-05 18:21:26 +0530

    added fibonacci problem using memoization

--- src/Recursion/Assignment1/Question11.java
+++ src/Recursion/Assignment1/Question11.java
@@ -1,9 +1,15 @@
 package Recursion.Assignment1;
 
+import java.util.HashMap;
+
 //Print nth number in fibonacci series using recursion
 public class Question11 {
+    public static HashMap<Integer,Long> hashMap = new HashMap<>();
     public static void main(String[] args) {
-        System.out.println(getNthFibonacci(0,1,7,1));
+//        System.out.println(getNthFibonacci(0,1,7,1));
+//        System.out.println(fibonacci(100));
+
+        System.out.println(fibonacciMemo(10));
     }
 
     public static long getNthFibonacci(long n1, long n2, int nth,int current){
@@ -17,14 +23,34 @@ public class Question11 {
         return  getNthFibonacci(n2,n1+n2,nth,current+1);
     }
 
-//    public static long fibonacci(int n){
-//        if(n==1){
-//            return 1;
-//        }
-//
-//        if(n==0){
-//            return 1;
-//        }
-//        return fibonacci(n-1) + fibonacci(n-2);
-//    }
+    //Using Backtracking
+    public static long fibonacci(int n){
+        if(n==1){
+            return 0;
+        }
+
+        if(n==2){
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static long fibonacciMemo(int n){
+        if(hashMap.containsKey(n)){
+            return hashMap.get(n);
+        }
+        if(n==1){
+            hashMap.put(1,0L);
+            return 0L;
+        }
+        if(n==2){
+            hashMap.put(2,1L);
+            return 1L;
+        }
+
+        long num = fibonacciMemo(n-1)+fibonacciMemo(n-2);
+        System.out.println(n+" : "+num);
+        hashMap.put(n,num);
+        return num;
+    }
 }
